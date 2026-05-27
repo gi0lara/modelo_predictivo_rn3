d20321d5b5544911ffe63fad1823ca5a1e3e3c68
Giovanna Lara <giovlara8@gmail.com>
2026-05-27 19:08:33 -0300

    Primer commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicción de baja visibilidad y riesgo vial en la RN3',
+    author='LARA Giovanna',
+    license='MIT',
+)
